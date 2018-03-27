99b06bbe1f4fb2099efa981d9a6f90eb3c947cc7
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2018-03-26 23:49:53 -0600

    init project

---
+++ quotes_root/env_quotes/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/hectorip/Development/python/TestingDjango/quotes_root/env_quotes/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
